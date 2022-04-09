c0ceaa1afbe4f1fd0eb9de0e8befbd29bf55e26f
tesla59 <talktonishantsingh.ns@gmail.com>
2022-04-09 15:52:10 +0530

    functions: import meme.jpeg image

--- functions.go
+++ functions.go
@@ -5,4 +5,6 @@ func LoadImages() {
 	router.StaticFile("/ayush.jpeg", "./front/images/ayush.jpeg")
 	router.StaticFile("/divyansh.jpeg", "./front/images/divyansh.jpeg")
 	router.StaticFile("/tesla.jpeg", "./front/images/tesla.jpeg")
+	router.StaticFile("/conf.jpeg", "./front/images/CONFESSION.jpeg")
+	router.StaticFile("/meme.jpeg", "/front/images/meme.jpeg")
 }
